f1632b2b23daa1e8d4a830e72afc77a8f3acd47f
michellewinata <michellewinata26@gmail.com>
2026-05-06 14:04:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -270,30 +270,23 @@ def load_and_train():
         file_id_1 = "1zWjvbR4WBEMZ7Gipz9nbQ26cSLuW8hi3"
         path1 = "dataset1_gojek.csv"
 
-        if not os.path.exists(path1):
-            url1 = f"https://drive.google.com/uc?id={file_id_1}"
-            gdown.download(url1, path1, quiet=False, fuzzy=True)
+    if not os.path.exists(path1):
+        url1 = f"https://drive.google.com/uc?export=download&id={file_id_1}"
+        gdown.download(url1, path1, quiet=False)
 
-        df1 = pd.read_csv(path1).dropna().drop_duplicates()
-        df1['clean_review'] = df1['review'].apply(clean_text)
-        df1['processed_review'] = df1['clean_review'].apply(preprocess_text)
-        df1['label'] = df1['rate'].map({'positive': 1, 'negative': 0})
+    if not os.path.exists(path1):
+        raise Exception("Dataset 1 gagal didownload!")
 
-        # DATASET 2
-        file_id_2 = "1URNmAxxjCzuYvRDnl6FLOtNbjZ9uIS2R"
-        path2 = "dataset2_gojek.csv"
+    # DATASET 2
+    file_id_2 = "1URNmAxxjCzuYvRDnl6FLOtNbjZ9uIS2R"
+    path2 = "dataset2_gojek.csv"
 
-        if not os.path.exists(path2):
-            url2 = f"https://drive.google.com/uc?id={file_id_2}"
-            gdown.download(url2, path2, quiet=False, fuzzy=True)
+    if not os.path.exists(path2):
+        url2 = f"https://drive.google.com/uc?export=download&id={file_id_2}"
+        gdown.download(url2, path2, quiet=False)
 
-        df2 = pd.read_csv(path2)
-        df2.columns = df2.columns.str.strip()
-
-        df2 = df2[df2['score'] != 3]
-        df2['label'] = df2['score'].apply(lambda x: 0 if x <= 2 else 1)
-        df2['clean_review'] = df2['content'].apply(clean_text)
-        df2['processed_review'] = df2['clean_review'].apply(preprocess_text)
+    if not os.path.exists(path2):
+        raise Exception("Dataset 2 gagal didownload!")
 
         results = {}
         for name, df, text_col in [("Dataset 1", df1, 'processed_review'), ("Dataset 2", df2, 'processed_review')]:
